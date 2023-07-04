ccd0ab8710bad8512eb68f8664c98e883505b330
Shan <shan.skog@gmail.com>
2023-07-04 21:32:03 +0800

    Integrate sonarqube with jest and eslint

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -6,7 +6,13 @@ scanner(
   serverUrl: "http://localhost:9000",
   token: process.env.REACT_APP_SONARQUBE_TOKEN,
   options: {
-    "sonar.sources": "./src"
+    "sonar.sources": "./src",
+    "sonar.exclusions": "**/*.test.tsx",
+    "sonar.tests": "./src",
+    "sonar.test.inclusions": "**/*.test.tsx,**/*.test.ts",
+    "sonar.typescript.lcov.reportPaths": "coverage/lcov.info",
+    "sonar.testExecutionReportPaths": "test-report.xml",
+    "sonar.eslint.reportPaths":"eslint-report.json"
   },
 },
 () => process.exit()
